d8f1da4d3f93a3565a67c3502db6996759e864a2
Slacker201 <slacker1234567809@gmail.com>
2026-08-13 22:29:04 -0600

    - Add basic lexer errors - Add chapter 1 & 2 tokens

---
+++ core/src/lexer_core/lex_tokens.rs
@@ -0,0 +1,56 @@
+use crate::core::Span;
+
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct LexToken {
+    span: Span,
+    kind: LexTokenKind,
+}
+
+impl LexToken {
+    pub fn new(span: Span, kind: LexTokenKind) -> Self {
+        Self { span, kind }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub enum LexTokenKind {
+    KeyWord(KeyWord),
+    Symbol(Symbol),
+    Constant(Constant),
+    Identifier(Identifier),
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub enum KeyWord {
+    Int,
+    Void,
+    Return,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub enum Symbol {
+    OpenParen,
+    CloseParen,
+    OpenBracket,
+    CloseBracket,
+    SemiColon,
+    Minus,
+    Tilda,
+    Decrement,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub enum Constant {
+    I32(i32),
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct Identifier {
+    ident: String,
+}
+
+impl Identifier {
+    pub fn new(ident: String) -> Self {
+        Self { ident }
+    }
+}

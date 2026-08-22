8dac94a68f5169743ddc789361816018d8b79d8a
Slacker201 <slacker1234567809@gmail.com>
2026-08-21 22:52:52 -0600

    - Implement new and generate_tacky for TackyGenerator

--- tacky_gen/src/generator/gen_implementations/tests.rs
+++ tacky_gen/src/generator/gen_implementations/tests.rs
@@ -1,16 +1,17 @@
 use lexer::Lexer;
 use parser::Parse;
-use slk_c_core::{parser_core::parser_ir::{Expression, Function, Statement}, tacky_core::{Label, TackyInstruction, Value, VariableId}};
+use slk_c_core::{
+    parser_core::parser_ir::{Function, Statement},
+    tacky_core::{Label, TackyInstruction, Value, VariableId},
+};
 use slk_tokenstream::TokenStream;
 
 use crate::generator::TackyGen;
 
-
-
 #[test]
 fn statement_correct_minimal_expr() {
     let expr: Vec<_> = "return 0;".chars().collect();
-    let tokens = Lexer::new(& expr).lex_until_error().unwrap();
+    let tokens = Lexer::new(&expr).lex_until_error().unwrap();
     let mut ts = TokenStream::new(&tokens);
     let expr = Statement::parse(&mut ts).unwrap();
 
@@ -26,14 +27,20 @@ fn statement_correct_minimal_expr() {
 #[test]
 fn statement_correct_three_token_expr() {
     let expr: Vec<_> = "return ~-0;".chars().collect();
-    let tokens = Lexer::new(& expr).lex_until_error().unwrap();
+    let tokens = Lexer::new(&expr).lex_until_error().unwrap();
     let mut ts = TokenStream::new(&tokens);
     let expr = Statement::parse(&mut ts).unwrap();
 
     let desired_tacky = vec![
         TackyInstruction::LoadVariable(VariableId::Variable { id: 0 }, Value::I32(0)),
-        TackyInstruction::Negate(VariableId::Variable { id: 1 }, VariableId::Variable { id: 0 }),
-        TackyInstruction::BitwiseNegate(VariableId::Variable { id: 2 }, VariableId::Variable { id: 1 }),
+        TackyInstruction::Negate(
+            VariableId::Variable { id: 1 },
+            VariableId::Variable { id: 0 },
+        ),
+        TackyInstruction::BitwiseNegate(
+            VariableId::Variable { id: 2 },
+            VariableId::Variable { id: 1 },
+        ),
         TackyInstruction::CopyValue(VariableId::EAX, VariableId::Variable { id: 2 }),
         TackyInstruction::Ret,
     ];
@@ -44,14 +51,20 @@ fn statement_correct_three_token_expr() {
 #[test]
 fn statement_correct_three_token_nested_expr() {
     let expr: Vec<_> = "return ~(-(0));".chars().collect();
-    let tokens = Lexer::new(& expr).lex_until_error().unwrap();
+    let tokens = Lexer::new(&expr).lex_until_error().unwrap();
     let mut ts = TokenStream::new(&tokens);
     let expr = Statement::parse(&mut ts).unwrap();
 
     let desired_tacky = vec![
         TackyInstruction::LoadVariable(VariableId::Variable { id: 0 }, Value::I32(0)),
-        TackyInstruction::Negate(VariableId::Variable { id: 1 }, VariableId::Variable { id: 0 }),
-        TackyInstruction::BitwiseNegate(VariableId::Variable { id: 2 }, VariableId::Variable { id: 1 }),
+        TackyInstruction::Negate(
+            VariableId::Variable { id: 1 },
+            VariableId::Variable { id: 0 },
+        ),
+        TackyInstruction::BitwiseNegate(
+            VariableId::Variable { id: 2 },
+            VariableId::Variable { id: 1 },
+        ),
         TackyInstruction::CopyValue(VariableId::EAX, VariableId::Variable { id: 2 }),
         TackyInstruction::Ret,
     ];
@@ -62,15 +75,21 @@ fn statement_correct_three_token_nested_expr() {
 #[test]
 fn function_correct_three_token_nested_expr() {
     let expr: Vec<_> = "int main(void) { return ~(-(0)); }".chars().collect();
-    let tokens = Lexer::new(& expr).lex_until_error().unwrap();
+    let tokens = Lexer::new(&expr).lex_until_error().unwrap();
     let mut ts = TokenStream::new(&tokens);
     let expr = Function::parse(&mut ts).unwrap();
 
     let desired_tacky = vec![
         TackyInstruction::Label(Label::new(0)),
         TackyInstruction::LoadVariable(VariableId::Variable { id: 0 }, Value::I32(0)),
-        TackyInstruction::Negate(VariableId::Variable { id: 1 }, VariableId::Variable { id: 0 }),
-        TackyInstruction::BitwiseNegate(VariableId::Variable { id: 2 }, VariableId::Variable { id: 1 }),
+        TackyInstruction::Negate(
+            VariableId::Variable { id: 1 },
+            VariableId::Variable { id: 0 },
+        ),
+        TackyInstruction::BitwiseNegate(
+            VariableId::Variable { id: 2 },
+            VariableId::Variable { id: 1 },
+        ),
         TackyInstruction::CopyValue(VariableId::EAX, VariableId::Variable { id: 2 }),
         TackyInstruction::Ret,
     ];
